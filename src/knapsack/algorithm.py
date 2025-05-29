06523d1510c10e9fbb872e48c8313bbb8c17db88
Cameron <cameronwood858@googlemail.com>
2025-05-29 20:37:24 +0100

    Add __repr__ .

--- src/knapsack/algorithm.py
+++ src/knapsack/algorithm.py
@@ -26,3 +26,14 @@ class KnapsackGA:
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
         self.elitism_rate = elitism_rate
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"num_items={self.num_items}, "
+            f"max_weight={self.max_weight}, "
+            f"population_size={self.population_size}, "
+            f"crossover_rate={self.crossover_rate:.2f}, "
+            f"mutation_rate={self.mutation_rate:.2f}, "
+            f"elitism_rate={self.elitism_rate:.2f})"
+        )
