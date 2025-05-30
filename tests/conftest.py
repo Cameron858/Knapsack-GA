0138b4b78a1490c38073195a56456a662f38d9a5
Cameron <cameronwood858@googlemail.com>
2025-05-30 10:03:13 +0100

    Add pytest fixture to return a small list of test items.

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from knapsack import Item
+
+
+@pytest.fixture
+def items() -> list[Item]:
+    """Fixture providing a small list of test items."""
+    return [
+        Item(name="item1", value=10, weight=5),
+        Item(name="item2", value=5, weight=3),
+        Item(name="item3", value=15, weight=9),
+        Item(name="item4", value=7, weight=4),
+        Item(name="item5", value=6, weight=2),
+    ]
